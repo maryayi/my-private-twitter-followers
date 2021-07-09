44574db278c4b2ad988fe56545416c15d79165fb
Mahdi Aryayi <mahdiaryayi@gmail.com>
2021-07-09 13:03:33 +0430

    Added next auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      Hello
+    <div className="container mx-auto">
+      <h1 className="text-center text-blue-700 text-2xl">My private twitter followers</h1>
+      <button className="text-white bg-blue-900 hover:bg-blue-500 rounded p-2">Login with Twitter</button>
     </div>
   )
 }
